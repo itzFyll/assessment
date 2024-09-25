8b115d841d4bcf02434a88130b93e926d9cee1da
admin <philipe.hamel@gmail.com>
2024-09-25 15:31:18 -0400

    Initial commit

---
+++ aodoc-assessment-app/src/test/java/org/aodocs/aodocassessmentapp/AodocAssessmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aodocs.aodocassessmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AodocAssessmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
